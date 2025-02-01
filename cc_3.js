94a224d8f9f3913e5c3ca6126c3f6307eb94dedc
Derek Ruize <derekruiz@usf.edu>
2025-02-01 14:16:46 -0500

    Task 1 - Product Price Management

---
+++ cc_3.js
@@ -0,0 +1,5 @@
+// Task 1 
+let prices = [70, 250, 1000, 25,150]; //Original Array
+prices.push(35); //Adds 35 to the array
+let updatedPrice = prices.slice(1); //Removes First Price
+console.log(updatedPrice); 
