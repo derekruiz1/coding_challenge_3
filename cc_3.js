2f0d4810266f271e4902ac8b4b19d9aefde94ecb
Derek Ruize <derekruiz@usf.edu>
2025-02-01 14:37:56 -0500

    Task 2 - Modifying Customer Orders

--- cc_3.js
+++ cc_3.js
@@ -3,3 +3,10 @@ let prices = [70, 250, 1000, 25,150]; //Original Array
 prices.push(35); //Adds 35 to the array
 let updatedPrice = prices.slice(1); //Removes First Price
 console.log(updatedPrice); 
+
+//Task 2
+let numbers = [13, 27, 10, 8, 36]; //Original Array
+numbers[2] += 5; //Adding 5 to the Third Number in the array
+let total = numbers.reduce((sum, numbers) => sum + numbers,0); //Adding up the numbers
+console.log(numbers);
+console.log(total);
